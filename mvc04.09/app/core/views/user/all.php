a4bcd20a90b7e5f01c16749d149c97e6a51025d9
WarrenMandarine <124674902+WarrenMandarine@users.noreply.github.com>
2023-09-05 17:24:41 +0200

    ajout
    ajout de commentaire

--- mvc04.09/app/core/views/user/all.php
+++ mvc04.09/app/core/views/user/all.php
@@ -1,9 +1,10 @@
 <?php 
     $pageUser = "Nos utilisateurs";
 ?>
-
+<!-- Affichage du titre principal -->
 <h1>Les ouvrages de notre bibliothèque&nbsp;:</h1>
 
+<!-- Affichage du tableau pour afficher les utilisateurs renseignés -->
 <table border=1>
         <thead>
             <th>Nom</th>
@@ -13,18 +14,22 @@
             <th>Supprimer</th>
         </thead>
         <tbody>
+            <!-- Boucle pour afficher les utilisateurs -->
             <?php foreach($results as $user){ ?>
                 <tr>
+                     <!-- Affichage des données de l'utilisateur -->
                     <td><?= $user["nom"]; ?></td>
                     <td><?= $user["prenom"]; ?></td>
                     <td><?= $user["email"]; ?></td>
                     <td>
+                        <!-- Formulaire pour la mise à jour de l'utilisateur -->
                         <form method="POST" action="index.php?controller=user&action=userUpdateForm">
                             <input type="hidden" value="<?= $user["id"]; ?>" name="updateID">
                             <input type="submit" value="📝">
                         </form>
                     </td>
                     <td>
+                        <!-- Formulaire pour la suppression de l'utilisateur -->
                         <form method="POST" action="index.php?controller=user&action=deleteUser">
                             <input type="hidden" value="<?= $user["id"]; ?>" name="deleteID">
                             <input type="submit" value="🗑️">
